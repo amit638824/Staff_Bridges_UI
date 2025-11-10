9d8c8c2c80a75f95da301881b8e21ae6f8a07b62
amit638824 <amit.chauhan@techwagger.com>
2025-11-10 17:17:46 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
+  reactCompiler: true,
 };
 
 export default nextConfig;
