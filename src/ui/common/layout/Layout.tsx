51ed7fc98ff898669d8bf68ea6db3636bbdd02e9
amit638824 <amit.chauhan@techwagger.com>
2025-12-15 19:26:10 +0530

    new

--- src/ui/common/layout/Layout.tsx
+++ src/ui/common/layout/Layout.tsx
@@ -1,19 +1,33 @@
-"use client";
-import React from "react";
+"use client"; 
+import React, { useEffect, useState } from "react";
 import SideBar from "./SideBar";
 import TopBar from "./TopBar";
+import { useSession } from "@/hooks/useSession";
+import { useRouter } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = useState(true);
+  const session = useSession();
+  const router = useRouter(); 
+  
+  useEffect(() => { 
+    if (session === null) {
+      router.replace("/");
+    }
+  }, [session, router]);
+ 
 
   return (
     <div className="d-flex vh-100">
       {/* Sidebar */}
-      <SideBar isOpen={open} toggle={() => setOpen(!open)} /> 
+      <SideBar isOpen={open} toggle={() => setOpen(!open)} />
+
       {/* Main */}
       <div className="flex-grow-1 d-flex flex-column">
-        <TopBar   />
-        <div className="p-4 flex-grow-1 overflow-auto">{children}</div>
+        <TopBar />
+        <div className="p-4 flex-grow-1 overflow-auto">
+          {children}
+        </div>
       </div>
     </div>
   );
